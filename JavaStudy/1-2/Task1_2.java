997e3f1592541be8021f7e59b62f503b1b3e19fe
ryohe1suzuki <95619156+ryohe1suzuki@users.noreply.github.com>
2022-01-17 23:17:24 +0900

    commit

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -1,5 +1,3 @@
-import java.time.chrono.JapaneseChronology;
-
 public class Task1_2{
     public static void main(String[] args) {
       // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
@@ -13,7 +11,10 @@ public class Task1_2{
          *   2番目の要素に 「もも」 を代入
          *   3番目の要素に 「ぶどう」 を代入
          */
-        String[] strArray = {"りんご", "もも", "ぶどう"};
+        String[] strArray = new String[3];
+        strArray[0] = "りんご";
+        strArray[1] = "もも";
+        strArray[2] ="ぶどう";
 
         // ④ ③で作成した配列の2番目の要素を出力しなさい。
         System.out.println(strArray[1]);
@@ -22,12 +23,7 @@ public class Task1_2{
          *  10,20,30,40,50を要素の値とする配列　intArrayを作成
          */
         int[] intArray = { 10, 20, 30, 40, 50 };
-
-        // ⑥下記の処理について、何をしているのかコメントを記入してください。
-        // 上記で作成した配列の２番目（20）、5番目（40）を加算（70)
+       // 上記で作成した配列の２番目（20）、5番目（40）を加算（70)
         System.out.println(intArray[1] + intArray[4]);
-
-
     }
-
 }
