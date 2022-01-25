5c21da74df582918de42aa24b9e57a56bde82d25
ryohe1suzuki <95619156+ryohe1suzuki@users.noreply.github.com>
2022-01-25 23:05:27 +0900

    commit

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -14,11 +14,11 @@ public class Task1_4 {
     String name = "alice" ;
     String pass = "alice123";
 //if構文の場合
-    if(name.equals("alice") && pass.equals("alice123")) {
+    if(name.equals(USER_NAME) && pass.equals(USER_PASSWORD)) {
         System.out.println(CONST_MSG_SUCCESS);
-    } else if (name.equals("alice") && !(pass.equals("alice123"))) {
+    } else if (name.equals(USER_NAME) && !(pass.equals(USER_PASSWORD))) {
         System.out.println(CONST_MSG_ERROR_PASS);
-    } else if (!(name.equals("alice")) && pass.equals("alice123")) {
+    } else if (!(name.equals(USER_NAME)) && pass.equals(USER_PASSWORD)) {
         System.out.println(CONST_MSG_ERROR_NAME);
     } else {
         System.out.println(CONST_MSG_ERROR_INPUT);
